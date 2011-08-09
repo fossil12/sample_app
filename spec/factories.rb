187a815177f73e81bedc72423581205df0ec10fc
David Keller <dev@davidkeller.me>
2011-08-10 00:34:01 +0200

    User model with passwords, and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+# By sing the symbol ':user', we get Factory Girl to simulate the User model.
+Factory.define :user do |user|
+	user.name						"David Keller"
+	user.email						"dev@davidkeller.me"
+	user.password					"foobar"
+	user.password_confirmation	"foobar"
+end
