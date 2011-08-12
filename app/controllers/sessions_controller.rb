a2e145b1135983937b8436054092ae295067f59a
David Keller <dev@davidkeller.me>
2011-08-12 23:58:16 +0200

    Handling invalid signin
    * Added webrat.log to .gitignore

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,19 @@ class SessionsController < ApplicationController
 	end
 	
 	def create 
-	
+		
+		user = User.authenticate(params[:session][:email], params[:session][:password])
+		
+		if user.nil?
+		
+			flash.now[:error] = "Invalid email/password combination."
+			@title = 'Sign in'
+			render 'new'
+			
+		else
+			# Sign in the user in and redirect to the user's show page.
+		end
+		
 	end
 	
 	def destroy
